aa35237c59e42bc332ce100b2007f5271206ba11
Bill Clark <bill@billclark.io>
2023-03-21 11:18:22 -0400

    Update

--- build.sh
+++ build.sh
@@ -11,26 +11,26 @@ else
 fi
 
 # Configure console colors & timestamp
-Blue='\033[0;34m'                       # Text color: Red
-Red='\033[0;31m'                        # Text color: Blue
-color_off='\033[0m'                     # Text color: Return to default
+Blue='\e[0;34m'                       # Text color: Red
+Red='\e[0;31m'                        # Text color: Blue
+color_off='\e[0m'                     # Text color: Return to default
 INFO="[${Blue}INFO${color_off}]"        # INFO tag
 ERRORTAG="[${Red}ERROR${color_off}]"    # ERROR tag
 TIMESTAMP="$(date '+%Y-%m-%dT%H:%M:%S %Z')"
 
 # Script Info
-echo "$INFO [$TIMESTAMP] This script was built on Ubuntu 22.04.2 LTS"
-echo "$INFO [$TIMESTAMP] You are currently running: $(head -1 /etc/os-release | cut -d "\"" -f 2)"
+echo -e "$INFO [$TIMESTAMP] This script was built on Ubuntu 22.04.2 LTS"
+echo -e "$INFO [$TIMESTAMP] You are currently running: $(head -1 /etc/os-release | cut -d "\"" -f 2)"
 
 # Log: Starting build
-echo "$INFO [$TIMESTAMP] Building image: $IMAGE_NAME:$TAG"
+echo -e "$INFO [$TIMESTAMP] Building image: $IMAGE_NAME:$TAG"
 
 # Build image
-docker build -t $IMAGE_NAME:$TAG .
+#docker build -t $IMAGE_NAME:$TAG .
 
 # Log: Success or failure
 if [ $? -eq 0 ]; then
-    echo "$INFO [$TIMESTAMP] Build complete: $IMAGE_NAME:$TAG"
+    echo -e "$INFO [$TIMESTAMP] Build complete: $IMAGE_NAME:$TAG"
 else
-    echo "$ERRORTAG [$TIMESTAMP] Build failed. See output for more information."
+    echo -e "$ERRORTAG [$TIMESTAMP] Build failed. See output for more information."
 fi
